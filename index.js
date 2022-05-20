d32b0c4af5161e5d09058c01bdd84c2c2eaf73fe
cynricchiu <qiuxinyu0609@gmail.com>
2022-05-21 01:23:48 +0800

    bugfix: 类别匹配

--- index.js
+++ index.js
@@ -3,7 +3,13 @@ const server = require('./server');
 
 const dytt = new MovieCrawler({
 	name: '电影天堂',
-	indexPage: 'https://dy.dytt8.net/html/gndy/dyzz/list_23_1.html',
+	indexPages: {
+		最新电影: 'https://dy.dytt8.net/html/gndy/dyzz/list_23_1.html',
+		日韩电影: 'https://www.ygdy8.net/html/gndy/rihan/list_6_1.html',
+		欧美电影: 'https://www.ygdy8.net/html/gndy/oumei/list_7_1.html',
+		国内电影: 'https://www.ygdy8.net/html/gndy/china/list_4_1.html',
+		综合电影: 'https://www.ygdy8.net/html/gndy/jddy/list_63_1.html',
+	},
 	ruleCallback: url => {
 		const curIndex = Number(url.substring(url.lastIndexOf('_') + 1, url.lastIndexOf('.')));
 		const nextPage = `${url.slice(0, url.lastIndexOf('_'))}_${Number(curIndex) + 1}.html`;
@@ -15,5 +21,3 @@ dytt.start(10).then(() => {
 	// 将所有结果输出为html
 	server.render({ content: dytt.result, category: dytt.category });
 });
-
-// server.render(content);
